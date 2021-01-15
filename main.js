2887b8fd43da89f0ce9ddc38f61d70a46c44839d
TheSkyFather <maxim.80930@gmail.com>
2021-01-16 00:03:37 +0400

    Patch

---
+++ main.js
@@ -0,0 +1,6 @@
+require("cores/megacore")
+require("cores/oliCore")
+var mod = Vars.mods.locateMod("mineralmod");
+mod.meta.displayName = "[#7FFFD4]M[#AFEEEE]i[#00CED1]n[#48D1CC]e[#40E0D0]r[#66CDAA]a[#7FFFD4]l[#66CDAA]s";
+mod.meta.description = "[#7FFFD4]Immerse yourself in the world of scenic minerals and complex [brick]mechanisms.";
+mod.meta.author = "[white]The[#7FFFD4]SkyFather [white]and [brick]paulieg626";
--- scripts/main.js
+++
@@ -1,3 +0,0 @@
-
-require("cores/megacore")
-require("cores/oliCore")
