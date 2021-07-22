e8f2d50ca44b3b57755b3c4c70307bd4b7da6770
TheSkyFather <maxim.80930@gmail.com>
2021-07-22 15:17:46 +0400

    Final balance fix and correcting code. And many minor fixes

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,4 @@ var mod = Vars.mods.locateMod("mineralmod");
 mod.meta.displayName = "[#7FFFD4]M[#AFEEEE]i[#00CED1]n[#48D1CC]e[#40E0D0]r[#66CDAA]a[#7FFFD4]l[#66CDAA]s";
 mod.meta.description = "[#7FFFD4]Immerse yourself in the world of scenic minerals and complex [brick]mechanisms.\n\n[#808080]Переводы:[#FFFFFF]\nРусский\nEnglish";
 mod.meta.author = "[white]The[#7FFFD4]SkyFather [white]and [brick]paulieg626";
-mod.meta.version = "[#7FFFD4]0.9.6.1"
+mod.meta.version = "[#7FFFD4]0.9.6.1 Final"
