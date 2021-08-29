85cea585230c9da1abde1826086a261895c7a96a
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-08-29 20:19:29 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-require("cores/megacore")
-require("cores/oliCore")
-var mod = Vars.mods.locateMod("mineralmod");
+const mod = Vars.mods.locateMod("mineralmod");
 mod.meta.displayName = "[#7FFFD4]M[#AFEEEE]i[#00CED1]n[#48D1CC]e[#40E0D0]r[#66CDAA]a[#7FFFD4]l[#66CDAA]s";
-mod.meta.description = "[#7FFFD4]Immerse yourself in the world of scenic minerals and complex [brick]mechanisms.\n\n[#808080]Переводы:[#FFFFFF]\nРусский\nEnglish";
-mod.meta.author = "[white]The[#7FFFD4]SkyFather [white]and [brick]paulieg626";
-mod.meta.version = "[#7FFFD4]0.9.6.1 Final"
+mod.meta.description = "[#7FFFD4]Immerse yourself in the world of scenic minerals and complex [brick]mechanisms.\n\n[#808080]Переводы/Translations :[#FFFFFF]\nРусский\nEnglish";
+mod.meta.author = "[white]The[#7FFFD4]SkyFather [white], [brick]paulieg626 [white] and [red]Super[green]Jaba[red]2000";
+mod.meta.version = "[#7FFFD4]0.9.6.1 Final";
+
+require("cores");
